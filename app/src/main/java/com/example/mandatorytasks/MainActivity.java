b5a8a934dc1dd5ad879a6077f118d7407a801f2f
Henkkis <jere.henriksson@kymp.net>
2023-09-19 17:13:53 +0300

    Second commit, with features!

--- app/src/main/java/com/example/mandatorytasks/MainActivity.java
+++ app/src/main/java/com/example/mandatorytasks/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.mandatorytasks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addBtn = (Button) findViewById(R.id.button);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editTextNumber = (EditText) findViewById(R.id.editTextNumber);
+                EditText editTextNumber2 = (EditText) findViewById(R.id.editTextNumber2);
+                TextView textView = (TextView) findViewById(R.id.textView);
+
+                int num1 = Integer.parseInt(editTextNumber.getText().toString());
+                int num2 = Integer.parseInt(editTextNumber2.getText().toString());
+                int result = num1 + num2;
+                textView.setText("Result: " + result);
+            }
+        });
     }
 }
